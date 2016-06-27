16a7b34fde61a911bcfff9c830daeef11badbc51
ITChong <836058144@qq.com>
2016-06-27 08:48:56 +0800

    第二次更新

--- YCDropDownMenu/ViewController.m
+++ YCDropDownMenu/ViewController.m
@@ -22,7 +22,7 @@
     self.view.backgroundColor = [UIColor lightGrayColor];
     
 #pragma mark - 例子一，有左边标题
-    YCDropDownMenu *menu1 = [[YCDropDownMenu alloc] initWithFrame:CGRectMake(0, 60, self.view.frame.size.width, 45)];
+    YCDropDownMenu *menu1 = [[YCDropDownMenu alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 45)];
     // 设置数据
     NSArray *listArray1 = @[ @"身份证", @"驾驶证", @"身份证", @"驾驶证", @"身份证", @"驾驶证"];
     // 设置代理
@@ -33,13 +33,30 @@
     
 #pragma mark - 例子二，没有左边标题
     YCDropDownMenu *menu2 = [[YCDropDownMenu alloc] init];
-    menu2.frame = CGRectMake(0, 120, self.view.frame.size.width, 45);
+    menu2.frame = CGRectMake(0, 50, self.view.frame.size.width, 45);
     // 设置数据
     NSArray *listArray2 = @[ @"身份证", @"驾驶证", @"身份证", @"驾驶证", @"身份证", @"驾驶证"];
     // 设置代理
     menu2.delegate = self;
     menu2.lists = listArray2;
     [self.view addSubview:menu2];
+    
+#pragma mark - 例子三
+    int cols = 3;
+    CGFloat menuY = 100;
+    CGFloat menuW = self.view.frame.size.width / cols;
+    CGFloat menuH = 45;
+    for (int i = 0; i < cols; i++) {
+        CGFloat menuX = i % cols * menuW;
+        YCDropDownMenu *deopDownMenu = [[YCDropDownMenu alloc] initWithFrame:CGRectMake(menuX, menuY, menuW, menuH)];
+        NSArray *listArray2 = @[ @"身份证", @"驾驶证", @"身份证", @"驾驶证", @"身份证", @"驾驶证"];
+        // 设置代理
+        deopDownMenu.delegate = self;
+        deopDownMenu.lists = listArray2;
+        [self.view addSubview:deopDownMenu];
+        
+    }
+    
     
 }
 
